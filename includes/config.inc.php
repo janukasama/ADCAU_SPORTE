709304e30baedd86461e9cc8092b70ac96eda0c3
Januka Samaranayake <jpsamaranayake@virtusa.com>
2015-08-07 12:25:00 +0530

    config

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,9 +1,9 @@
 <?php
-$DbHost	 = "localhost";
-$DbDatabase = "vbid";
-$DbUser	 = "root";
-$DbPassword = "";
+$DbHost	 = "us-cdbr-iron-east-02.cleardb.net";
+$DbDatabase = "heroku_1cf2af801e7beba";
+$DbUser	 = "b559db8f019765";
+$DbPassword = "3514024a";
 $DBPrefix	= "webid_";
-$main_path	= "C:\\xampp\\htdocs\\ADCAU_SPORTE\\";
-$MD5_PREFIX = "680799e06fb8fa59dcda9d2fa76c910c";
+$main_path	= "/app/";
+$MD5_PREFIX = "randomcodepubz";
 ?>
